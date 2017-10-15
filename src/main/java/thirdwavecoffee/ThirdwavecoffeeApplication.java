034ee6c9526c37b99006153f6981bfc960961a46
Alp Çelik <alparslancelik@anadolu.edu.tr>
2017-10-15 19:21:38 +0300

    Initial commit

---
+++ src/main/java/thirdwavecoffee/ThirdwavecoffeeApplication.java
@@ -0,0 +1,12 @@
+package thirdwavecoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdwavecoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdwavecoffeeApplication.class, args);
+	}
+}
